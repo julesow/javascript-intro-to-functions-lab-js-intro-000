2415068734523c635aa85326c5e2d7bdd374f198
SOULEYMANE SOW <souleymanesow@gmail.com>
2018-10-10 10:35:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if (whisper(string)){return "I can't hear you!";}
-  else if (shout()){ return "YES INDEED!";}
-  else (string === "I love you, Grandma."){return "I love you, too.";}
+  if (string === string.toLowerCase()){return "I can't hear you!"}
+  else if (string === string.toUpperCase()){ return "YES INDEED!"}
+  else (string === "I love you, Grandma.") return "I love you, too."
   
 }
